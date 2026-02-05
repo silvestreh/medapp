5515ccf359f8f047328eb18f966d3b0ca18e4c54
Silvestre <hello@silvestre.io>
2026-02-05 20:42:57 -0300

    Reduce sample rate

--- apps/ui/app/entry.client.tsx
+++ apps/ui/app/entry.client.tsx
@@ -8,7 +8,7 @@ import { ClientCacheProvider } from '~/stitches';
 
 Sentry.init({
   dsn: 'https://d000ab2531d759f74d2cbd4257414635@o4508344607834112.ingest.de.sentry.io/4508344611569744',
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
   integrations: [
     Sentry.browserTracingIntegration({
