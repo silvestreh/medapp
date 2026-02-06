3848bbc35e7f3aba759c2ad6490eba73d24d7361
Silvestre <hello@silvestre.io>
2026-02-05 21:51:34 -0300

    Add patient search and details

---
+++ apps/ui/app/routes/encounters._index.tsx
@@ -0,0 +1,68 @@
+import dayjs from 'dayjs';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import { type LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { Flex } from '@mantine/core';
+
+import { generateSlots } from '~/utils';
+import { styled } from '~/stitches';
+import { getAuthenticatedClient, authenticatedLoader } from '~/utils/auth.server';
+import AppointmentsList from '~/components/appointments-list';
+import PatientSearchTable from '~/components/patient-search-table';
+
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
+
+const Container = styled(Flex, {
+  '@sm': {
+    flexDirection: 'column',
+  },
+  '@md': {
+    flexDirection: 'row',
+    padding: '1rem',
+  },
+  gap: '1rem',
+});
+
+const LeftColumn = styled(Flex, {
+  '@md': {
+    width: '40%',
+  },
+});
+
+const MainColumn = styled(Flex, {
+  flex: 1,
+});
+
+export const loader = authenticatedLoader(async ({ request }: LoaderFunctionArgs) => {
+  const { client, user } = await getAuthenticatedClient(request);
+  const date = dayjs('2024-11-27');
+  const appointments = await client.service('appointments').find({
+    query: {
+      medicId: user?.id,
+      startDate: {
+        $gte: date.startOf('month').toISOString(),
+        $lte: date.endOf('month').toISOString(),
+      },
+    },
+  });
+  const slots = generateSlots(date, appointments, user);
+
+  return { slots };
+});
+
+export default function EncountersIndex() {
+  const { slots } = useLoaderData<typeof loader>();
+
+  return (
+    <Container>
+      <LeftColumn>
+        <AppointmentsList slots={slots} readonly onAppointmentClick={console.log} />
+      </LeftColumn>
+      <MainColumn>
+        <PatientSearchTable />
+      </MainColumn>
+    </Container>
+  );
+}
--- apps/ui/app/routes/encounters.tsx
+++ apps/ui/app/routes/encounters.tsx
@@ -1,18 +1,6 @@
-import dayjs from 'dayjs';
-import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
-import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
-import { type LoaderFunctionArgs, type MetaFunction, type LinksFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
-import { Flex } from '@mantine/core';
+import { type LinksFunction, type MetaFunction } from '@remix-run/node';
+import { Outlet } from '@remix-run/react';
 import datesStyles from '@mantine/dates/styles.css?url';
-
-import { generateSlots } from '~/utils';
-import { styled } from '~/stitches';
-import { getAuthenticatedClient, authenticatedLoader } from '~/utils/auth.server';
-import AppointmentsList from '~/components/appointments-list';
-
-dayjs.extend(isSameOrAfter);
-dayjs.extend(isSameOrBefore);
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: datesStyles }];
 
@@ -20,38 +8,6 @@ export const meta: MetaFunction = () => {
   return [{ title: 'MedApp / Encuentros' }];
 };
 
-const Container = styled(Flex, {
-  '@sm': {
-    flexDirection: 'column',
-  },
-  '@md': {
-    flexDirection: 'row',
-  },
-});
-
-export const loader = authenticatedLoader(async ({ request }: LoaderFunctionArgs) => {
-  const { client, user } = await getAuthenticatedClient(request);
-  const date = dayjs('2024-11-27');
-  const appointments = await client.service('appointments').find({
-    query: {
-      medicId: user?.id,
-      startDate: {
-        $gte: date.startOf('month').toISOString(),
-        $lte: date.endOf('month').toISOString(),
-      },
-    },
-  });
-  const slots = generateSlots(date, appointments, user);
-
-  return { slots };
-});
-
 export default function Encounters() {
-  const { slots } = useLoaderData<typeof loader>();
-
-  return (
-    <Container>
-      <AppointmentsList slots={slots} readonly onAppointmentClick={console.log} />
-    </Container>
-  );
+  return <Outlet />;
 }
