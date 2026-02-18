0c7a7ec85e1a8d501232dd21aeedf1efffb11184
Silvestre <hello@silvestre.io>
2026-02-18 19:46:15 -0300

    Fix studies seed protocol reset

--- apps/api/src/services/studies/hooks/auto-protocol.ts
+++ apps/api/src/services/studies/hooks/auto-protocol.ts
@@ -3,6 +3,10 @@ import { Sequelize, QueryTypes } from 'sequelize';
 
 export default function autoProtocol(): Hook {
   return async (context: HookContext) => {
+    if (context.data.protocol) {
+      return context;
+    }
+
     const sequelize: Sequelize = context.app.get('sequelizeClient');
 
     const [row] = await sequelize.query<{ max: number | null }>(
