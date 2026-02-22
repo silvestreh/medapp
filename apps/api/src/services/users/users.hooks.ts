d9a71b16ce726616e2eeff32e38c88585fa7c4fc
Silvestre <hello@silvestre.io>
2026-02-21 22:32:28 -0300

    scope users find to org

--- apps/api/src/services/users/users.hooks.ts
+++ apps/api/src/services/users/users.hooks.ts
@@ -1,3 +1,4 @@
+import { HookContext } from '@feathersjs/feathers';
 import * as feathersAuthentication from '@feathersjs/authentication';
 import * as local from '@feathersjs/authentication-local';
 import createPersonalData from '../../hooks/create-personal-data';
@@ -5,16 +6,39 @@ import createContactData from '../../hooks/create-contact-data';
 import includeData from '../../hooks/include-data';
 import populateUser from './hooks/populate-user';
 import { prepareSignupOrganization, handleSignupOrganization } from './hooks/handle-signup-organization';
+import { verifyOrganizationMembership } from '../../hooks/verify-organization-membership';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = feathersAuthentication.hooks;
 const { hashPassword, protect } = local.hooks;
 
+const scopeUsersToOrganization = () => async (context: HookContext): Promise<HookContext> => {
+  const { app, params } = context;
+
+  if (!params.provider || !params.organizationId) return context;
+
+  const memberships: any[] = await app.service('organization-users').find({
+    query: { organizationId: params.organizationId },
+    paginate: false,
+  } as any);
+
+  const userIds = memberships.map((m: any) => m.userId);
+
+  context.params.query = {
+    ...context.params.query,
+    id: { $in: userIds },
+  };
+
+  return context;
+};
+
 export default {
   before: {
     all: [],
     find: [
-      authenticate('jwt')
+      authenticate('jwt'),
+      verifyOrganizationMembership(),
+      scopeUsersToOrganization(),
     ],
     get: [
       authenticate('jwt')
